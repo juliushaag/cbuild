cba12cbae693d20e4706de3efbb11f244c669423
Julius Haag <haag.julius@outlook.de>
2024-03-01 21:38:41 +0100

    Switched to pure MSVC compilation

--- cbuild/processes.py
+++ cbuild/processes.py
@@ -12,14 +12,13 @@ class Program:
   def is_valid(self) -> bool:
     return shutil.which(self.program) or os.path.isfile(self.program)
   
-  def run(self, args : str) -> tuple[Any, Any, int]:
-    return self.run_async(args).wait()
+  def run(self, args : str = None, cwd=None) -> tuple[Any, Any, int]:
+    return self.run_async(args, cwd = cwd).wait()
   
-  def run_async(self, args : str) -> "Process":
+  def run_async(self, args : str, cwd = None) -> "Process":
     cmd = [self.program] + (args.split(" ") if args is not None else [])
-    link = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy(), universal_newlines=True)
-
-    return Process(self, link, cmd)
+    link = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy(), universal_newlines=True, cwd=cwd)
+    return Process(self, link, args=cmd)
 
   def __call__(self, args : str) -> tuple[Any, Any, int]:
     return self.run(args)
@@ -29,7 +28,7 @@ class Process:
   def __init__(self, program : Program, link : subprocess.Popen, args = []) -> None:
     self.program = program
     self.link = link
-    self.args : list = []
+    self.args : list = args
 
 
   def wait(self, timeout=None) -> tuple[Any, Any, int]:
--- cbuild/tools/msvc.py
+++ cbuild/tools/msvc.py
@@ -1,14 +1,13 @@
+from collections import deque
 from pathlib import Path
-import re
-import subprocess
-from typing import Generator, Optional
 from cbuild.compiler import CompileError, CompileResult, Compiler, ExeCompileResult, HeaderCompileResult, LibCompileResult, ObjCompileResult
 from cbuild.log import panic, error, success
 from cbuild.processes import Process, Program
 from cbuild.project import Target
 from glob import glob
-import os
 import time
+import os
+import re
 
 class MSVCCompiler(Compiler):
   NAME = "MSVC"
@@ -24,37 +23,48 @@ class MSVCCompiler(Compiler):
   def __call__(self, target : Target, res : LibCompileResult) -> CompileResult:
     assert target.type in self.type 
     #  CL [option...] file... [option | file]... [lib...] [@command-file] [/link link-opt...]
-    args = ["/nologo",  "/D _CRT_SECURE_NO_WARNINGS", "/Z7", "/c","/Tc" if target.type == "c" else "/Tp"]
+    args = ["/nologo", "/Z7"]
     includes = target.get("includes", [])
     sources = target.get("sources", []) 
     bin_dir = target.get("bin_dir", "bin/")
     kind = target.get("kind", None)
-
-
+    defines = target.get("defines", [])
+    debug = target.get("debug", False)
 
     if not os.path.isdir(target.root / bin_dir): os.mkdir(target.root / bin_dir)
+    
 
     sources = sources if isinstance(sources, list) else [sources]
     includes = includes if isinstance(includes, list) else [includes]
 
     includes = [target.root / include for include in includes] + res.includes
+    args = ["/D " + name for name in defines] + args
+
     
-    src_files = [file for source in sources for file in glob(source, root_dir=target.root, recursive=True)]
-
-    success(MSVCCompiler.NAME + " compiles " + target.name)
-
+    if kind == "header": return HeaderCompileResult(includes=includes)
 
     # TODO this is garbage 
     compiled_files = []
 
 
+    # Yeah this is a little scuffed due to early dispatch 
     start = time.monotonic()
-    result : list[tuple[Process, Path]] = [self._compile_file_start(target.root, file, bin_dir, args, includes) for file in src_files]
-    compiled = [self._compile_file_end(*end) for end in result]
-    for i in compiled:
+    running : deque[tuple[Process, Path]] = deque([self._compile_file_start(target.root, file, bin_dir, args, includes) for source in sources for file in glob(source, root_dir=target.root, recursive=True)])
+    finished = []
+
+    success(MSVCCompiler.NAME + " compiles " + target.name)
+
+    # polling the compile status
+    while running and (element := running.pop()):
+      process = element[0]
+      if process.hash_finished():
+        finished.append(self._compile_file_end(*element))
+      else:
+        running.append(element)
+
+    for i in finished:
       if i.error(): return i
       compiled_files += [i.get_file()]
-    print("Compiling the files took", time.monotonic() - start)
 
     if kind == "exe": 
       exe = self._compile_exe(compiled_files, res.static_lib, target.root / bin_dir, target.name)
@@ -65,9 +75,6 @@ class MSVCCompiler(Compiler):
       lib.includes = includes
       return lib
     
-    elif kind == "header":
-      return HeaderCompileResult(includes=includes)
-
     else: assert False
   
   def _compile_file_start(self, root : Path, file : str, bin_folder : Path, args : list[str], includes) -> tuple[Process, Path]:
@@ -75,22 +82,22 @@ class MSVCCompiler(Compiler):
     src_file = root / file
     includes = ["-I" + str(root / include) for include in includes] if includes is not None else []
     output_file = root / bin_folder / file.replace(".c", ".obj").replace("/", ".").replace("\\", ".")
-    command = f"/Fo{output_file} {" ".join(args)} {src_file} {" ".join(includes)}"
-
+    command = f"{" ".join(args)} /c /Tc {src_file} {" ".join(includes)} /Fo{output_file}"
     return (self.compiler.run_async(command), output_file)
   
   def _compile_file_end(self, process : Process, output_file : Path):
 
     out, err, code = process.wait()
-    if code != 0: return self._parse_error(out.split("\n", 1)[1])
+    if code != 0: 
+      return self._parse_error(out.split("\n", 1)[1])
     
-    print(f"{out.strip():20}")
+    print(out.strip())
     return ObjCompileResult(output_file)
   
   def _compile_exe(self, compiled, libs, out_path : Path, name : str) -> ExeCompileResult | CompileError:
 
     out_path = out_path / (name + ".exe")
-    cmd = f"/debug /OUT:{str(out_path)} {" ".join([str(lib) for lib in libs])} {" ".join([str(comp) for comp in compiled])} {" ".join([str(lib) for lib in MSVCCompiler.STD_LIBS])}"
+    cmd = f"/nologo /debug /OUT:{str(out_path)} {" ".join([str(lib) for lib in libs])} {" ".join([str(comp) for comp in compiled])} {" ".join([str(lib) for lib in MSVCCompiler.STD_LIBS])}"
     out, err, code = self.linker(cmd)
   
     if code != 0: return self._parse_error(err + out)
